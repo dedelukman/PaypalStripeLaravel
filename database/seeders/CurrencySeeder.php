43f52ed76150206da33d1ad6b0c3d1bb7cc1d633
dedelukman <dlukmanul.h@gmail.com>
2021-09-10 22:33:05 +0700

    make model migration seeder add img

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Currency;
+use Illuminate\Database\Seeder;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currencies = [
+            'usd',
+            'idr',
+            'eur',
+        ];
+
+        foreach($currencies as $currency){
+            Currency::create([
+                'iso' => $currency,
+            ]);
+        }
+    }
+}
